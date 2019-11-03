1c987239468e3b75fde34344671452c6330c9d21
Jack <kotzecabo@gmail.com>
2019-11-03 10:59:11 +0200

    Added Message Pack Deserialization

---
+++ Message.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MessagePack;
+
+namespace BleGatewayTest
+{
+    [MessagePackObject]
+    public class Message
+    {
+        [Key(0)]
+        public string v { get; set; }
+        [Key(1)]
+        public int mid { get; set; }
+        [Key(2)]
+        public int time { get; set; }
+        [Key(3)]
+        public string ip { get; set; }
+        [Key(4)]
+        public string mac { get; set; }
+        [Key(5)]
+        public ICollection<Device> devices { get; set; }
+    }
+
+    [MessagePackObject]
+    public class Device
+    {
+        [Key(0)]
+        public string type { get; set; }
+        [Key(1)]
+        public int[] data { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿using RestSharp;
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Net;
 using System.Threading;
-
+using MessagePack;
 using uPLibrary.Networking.M2Mqtt.Messages;
 using uPLibrary.Networking.M2Mqtt;
 using MqttClient = uPLibrary.Networking.M2Mqtt.MqttClient;
@@ -13,6 +14,47 @@ namespace BleGatewayTest
     class Program
     {
         static void Main(string[] args)
+        {
+            FireEvent();
+        }
+
+        public static void DeserializeTopic()
+        {
+
+        }
+        public static void TestMessagePackSerializer()
+        {
+            var message = new Message
+            {
+                v = "10.1.1.1.1",
+                mid = 211,
+                time = 345,
+                ip = "192.943.98.1",
+                mac = "kj0983kjadsfklj",
+                devices = new List<Device>()
+                {
+                    new Device
+                    {
+                        type = "Default",
+                        data = new []{21,32,34,54,54,245,53425,235,235,345,23,452,345,234,523,45}
+                    },
+                    new Device
+                    {
+                        type = "Default",
+                        data = new []{21,32,34,54,54,245,525,235,235,345,23,452,345,234,523,45}
+                    }
+                }
+            };
+
+            byte[] bytes = MessagePackSerializer.Serialize(message);
+            Console.WriteLine(bytes.Length);
+            foreach (var item in bytes)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadLine();
+        }
+        public static void FireEvent()
         {
             var client = new MqttClient("mqtt.bconimg.com");
 
@@ -27,17 +69,31 @@ namespace BleGatewayTest
                 new string[] { "merge01" },
                 new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
         }
-
         public static string Line { get; set; }
+
         static void client_MqttMsgPublishReceived(
             object sender, MqttMsgPublishEventArgs e)
         {
-            string result = System.Text.Encoding.UTF8.GetString(e.Message);
-            Console.WriteLine(result);
+            //string result = System.Text.Encoding.UTF8.GetString(e.Message);
+            //Console.WriteLine(result);
             // handle message received
+
+            var json = MessagePackSerializer.ToJson(e.Message);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            string[] first = json.Split(':');
+            foreach (var item in first)
+            {
+                Console.WriteLine(item);
+            }
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
+            Console.WriteLine();
+
             //foreach (var item in e.Message)
             //{
-            //    Line += item;
+            //    Line +=  item + ", ";
             //}
             ////
             //Console.WriteLine("message=" + Line);
